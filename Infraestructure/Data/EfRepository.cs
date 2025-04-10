ee526760dd06461f6f3246b15212e49872a32400
MauroGBecares <maurobecares26@gmail.com>
2025-04-10 18:20:50 -0300

    Repository pattern and IUserRepository

--- Infraestructure/Data/EfRepository.cs
+++ Infraestructure/Data/EfRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Infraestructure.Data
 {
-    internal class EfRepository
+    public class EfRepository<T> : RepositoryBase<T> where T : class
     {
+        protected readonly ApplicationDbContext _context;
+        public EfRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _context = dbContext;
+        }
     }
 }
