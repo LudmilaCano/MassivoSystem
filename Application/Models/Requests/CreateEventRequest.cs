0da38abaee5e90d3905debc4f8c2540aeb9150c8
ricardobiselli <ricardobiselli@gmail.com>
2025-05-24 22:22:00 -0300

    PPf-90 feat: validaciones DTOs Event

--- Application/Models/Requests/CreateEventRequest.cs
+++ Application/Models/Requests/CreateEventRequest.cs
@@ -1,14 +1,21 @@
 ﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application.Models.Requests
 {
     public class CreateEventRequest
     {
-        //agregar validaciones!
+        [Required(ErrorMessage = "UserId is required.")]
         public int UserId { get; set; }
+        [Required(ErrorMessage = "La ubicación es obligatoria")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Ubicación debe tener entre 4 y 20 caracteres")]
         public string Location { get; set; }
+        [Required(ErrorMessage = "Event name is required.")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "el nombre del evento debe tener entre 4 y 20 caracteres")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Date is required.")]
         public DateTime EventDate { get; set; }
+        [Required(ErrorMessage = "Type is required.")]
         public EventType Type { get; set; }
     }
 }
