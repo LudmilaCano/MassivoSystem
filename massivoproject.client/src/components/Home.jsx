9fa8c51babd911d325a4db3826f4f1206a67d6f2
Ren991 <beccrenzo@gmail.com>
2025-06-01 12:26:24 -0300

    add hook busyDialog

--- massivoproject.client/src/components/Home.jsx
+++ massivoproject.client/src/components/Home.jsx
@@ -11,6 +11,7 @@ import {
 import imagenEjemplo from '../images/logo2.png';
 import { getRandomEvents, filterEvents } from '../api/EventEndpoints';
 import { getEventTypeLabel } from '../constants/eventCategories';
+import { useBusyDialog } from '../hooks/useBusyDialog';
 
 const Home = () => {
 
@@ -18,6 +19,7 @@ const Home = () => {
     const [people, setPeople] = useState('');
     const [searchName, setSearchName] = useState('');
     const [searchDate, setSearchDate] = useState('');
+    const [busy, setBusy, BusyDialog] = useBusyDialog();
 
     const handlePeopleChange = (e) => {
         const value = e.target.value;
@@ -27,13 +29,16 @@ const Home = () => {
     };
 
     const handleFilter = async () => {
+        setBusy(true);
         try {
             const data = await filterEvents(searchName, searchDate);
             setEvents(data);
         } catch (error) {
             console.error('Error filtrando eventos:', error);
         }
+        setBusy(false);
     };
+
 
     function formatDate(dateString) {
         const date = new Date(dateString);
@@ -47,16 +52,18 @@ const Home = () => {
     
     useEffect(() => {
         const fetchEvents = async () => {
+            setBusy(true);
             try {
                 const data = await getRandomEvents(4);
                 setEvents(data);
             } catch (error) {
                 console.error('Error fetching random events:', error);
             }
+            setBusy(false);
         };
         fetchEvents();
     }, []);
-    console.log(events);
+
     const features = [
         { icon: '✅🚗', label: 'Viajá seguro', shadowColor: 'rgba(255, 69, 0, 0.5)' },
         { icon: '💸', label: 'Viajá barato', shadowColor: 'rgba(30, 144, 255, 0.5)' },
@@ -64,7 +71,9 @@ const Home = () => {
     ];
 
     return (
-        <Box sx={{ px: 8, py: 2, maxHeight: '70vh', }}>
+        <>
+        {BusyDialog}
+        {<Box sx={{ px: 8, py: 2, maxHeight: '70vh', }}>
             {/* Header */}
             <Typography variant="h5" fontWeight="bold" textAlign="center">
                 Compartí tu viaje, compartí la experiencia
@@ -182,7 +191,8 @@ const Home = () => {
                     </Grid>
                 ))}
             </Grid>
-        </Box>
+        </Box>}
+        </>
     );
 };
 
